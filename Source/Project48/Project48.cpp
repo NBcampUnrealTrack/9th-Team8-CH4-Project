bd776fcb6d79660d049497822b191604cbc20672
AntiFreeze81 <wnsgud3590@naver.com>
2026-08-24 19:23:21 +0900

    feat: 언리얼 기본 프로젝트 파일 추가

---
+++ Source/Project48/Project48.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project48.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project48, "Project48" );
