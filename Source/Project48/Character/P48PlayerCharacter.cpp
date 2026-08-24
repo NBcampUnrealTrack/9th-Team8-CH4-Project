a4cafc35411deaa545067392e13337823c72de5c
AntiFreeze81 <wnsgud3590@naver.com>
2026-08-24 21:11:17 +0900

    add: 기본 캐릭터, 컨트롤러 소스 및 BP 생성 (#5)

---
+++ Source/Project48/Character/P48PlayerCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "P48PlayerCharacter.h"
+
+
+// Sets default values
+AP48PlayerCharacter::AP48PlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AP48PlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AP48PlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AP48PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
