4d0c2e3c99defce85f33159fd29c6ec918fa38dd
Vactioned <won23245@gmail.com>
2026-08-30 21:37:54 +0900

    add: MatchmakingSubsystem 헤더 및 소스 파일 생성( #18 )

---
+++ Source/Project48/Online/P48MatchmakingSubsystem.cpp
@@ -0,0 +1,11 @@
+#include "P48MatchmakingSubsystem.h"
+
+void UP48MatchmakingSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UP48MatchmakingSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
