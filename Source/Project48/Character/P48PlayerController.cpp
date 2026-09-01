07362ceedf20516ac1bb43fe8a36a61e3de21153
AntiFreeze81 <wnsgud3590@naver.com>
2026-09-01 19:38:16 +0900

    feat: Test 버튼 UI 생성 및 Controller에서 ServerRPC로 Ready구현

--- Source/Project48/Character/P48PlayerController.cpp
+++ Source/Project48/Character/P48PlayerController.cpp
@@ -1,7 +1,10 @@
 ﻿#include "P48PlayerController.h"
 
+#include "P48PlayerState.h"
+
 #include "EnhancedInputSubsystems.h"
 #include "InputMappingContext.h"
+#include "Blueprint/UserWidget.h"
 
 void AP48PlayerController::BeginPlay()
 {
@@ -17,4 +20,51 @@ void AP48PlayerController::BeginPlay()
 			}
 		}
 	}
+	
+	WidgetInstance = CreateWidget<UUserWidget>(this, WidgetClass);
+	if (WidgetInstance)
+	{
+		WidgetInstance->AddToViewport();
+		
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void AP48PlayerController::RequestReady()
+{
+	AP48PlayerState* PS = GetPlayerState<AP48PlayerState>();
+	if (!PS)
+	{
+		return;
+	}
+	
+	if (PS->IsReady())
+	{
+		return;
+	}
+	
+	Server_SetReady(true);
+}
+
+bool AP48PlayerController::Server_SetReady_Validate(bool bNewReady)
+{
+	return GetPlayerState<AP48PlayerState>() != nullptr;
+}
+
+void AP48PlayerController::Server_SetReady_Implementation(bool bNewReady)
+{
+	AP48PlayerState* PS = GetPlayerState<AP48PlayerState>();
+	
+	if (!PS)
+	{
+		return;
+	}
+	
+	if (PS->IsReady())
+	{
+		return;
+	}
+	
+	PS->SetReady(bNewReady);
 }
