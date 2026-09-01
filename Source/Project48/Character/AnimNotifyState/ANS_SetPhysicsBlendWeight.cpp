c07ac744955f87414f3c087b5ceae4332602e501
AntiFreeze81 <wnsgud3590@naver.com>
2026-09-01 12:24:38 +0900

    feat: 노티파이 가중치 보간 사용으로 자연스러운 애니메이션 완성

--- Source/Project48/Character/AnimNotifyState/ANS_SetPhysicsBlendWeight.cpp
+++ Source/Project48/Character/AnimNotifyState/ANS_SetPhysicsBlendWeight.cpp
@@ -1,4 +1,36 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "ANS_SetPhysicsBlendWeight.h"
 
+void UANS_SetPhysicsBlendWeight::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+	
+	CurrentBlendWeight = DefaultPhysicsWeight;
+	
+}
 
-#include "ANS_SetPhysicsBlendWeight.h"
+void UANS_SetPhysicsBlendWeight::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyTick(MeshComp, Animation, FrameDeltaTime, EventReference);
+	
+	if (MeshComp)
+	{
+		if (!TargetBoneName.IsNone())
+		{
+			CurrentBlendWeight = FMath::FInterpTo(CurrentBlendWeight, AttackPhysicsWeight, FrameDeltaTime, InterpSpeed);
+			MeshComp->SetAllBodiesBelowPhysicsBlendWeight(TargetBoneName, CurrentBlendWeight, false, true);
+		}
+	}
+}
+
+void UANS_SetPhysicsBlendWeight::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+	
+	if (MeshComp)
+	{
+		if (!TargetBoneName.IsNone())
+		{
+			MeshComp->SetAllBodiesBelowPhysicsBlendWeight(TargetBoneName, DefaultPhysicsWeight, false, true);
+		}
+	}
+}
--- Source/Project48/Character/AnimNotifyState/ANS_SetPhysicsBlendWeight.h
+++ Source/Project48/Character/AnimNotifyState/ANS_SetPhysicsBlendWeight.h
@@ -1,6 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotifyState.h"
@@ -13,4 +11,25 @@ UCLASS()
 class PROJECT48_API UANS_SetPhysicsBlendWeight : public UAnimNotifyState
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	virtual void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	float AttackPhysicsWeight = 0.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	float DefaultPhysicsWeight = 0.8f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	float InterpSpeed = 15.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Physics")
+	FName TargetBoneName = FName("upperarm_r");
+	
+private:
+	float CurrentBlendWeight = 1.0f;
 };
