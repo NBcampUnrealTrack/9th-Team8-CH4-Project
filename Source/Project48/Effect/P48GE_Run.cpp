e1ea515550587ef2e248e6d28d66c55fc25c0c19
AntiFreeze81 <wnsgud3590@naver.com>
2026-09-03 15:39:46 +0900

    feat: GAS 적용 및 DT를 통한 데이터 기반 시스템 적용

---
+++ Source/Project48/Effect/P48GE_Run.cpp
@@ -0,0 +1,12 @@
+﻿#include "P48GE_Run.h"
+
+UP48GE_Run::UP48GE_Run()
+{
+	DurationPolicy = EGameplayEffectDurationType::Infinite;
+	
+	FGameplayTag RunTag = FGameplayTag::RequestGameplayTag(FName("State.Movement.Running"), false);
+	
+	FInheritedTagContainer TagContainer;
+	TagContainer.AddTag(RunTag);
+	InheritableOwnedTagsContainer = TagContainer;
+}
---
+++ Source/Project48/Effect/P48GE_Run.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "P48GE_Run.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT48_API UP48GE_Run : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+public:
+	UP48GE_Run();
+};
