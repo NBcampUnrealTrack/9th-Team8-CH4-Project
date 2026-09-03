194c4d68422c45db45ac3b055f7d9396f11e2d57
mooang lee <natalie12595@gmail.com>
2026-09-03 16:08:21 +0900

    feat: 생존 모드 플레이어 탈락 및 라운드 종료 판정 구현 ( #42 )
    - 생존 참가자 수 및 마지막 생존자 조회 기능 추가
    - 서버 기반 플레이어 탈락 처리 함수 추가
    - 관전자 및 중복 탈락 요청 방지
    - 동시 사망을 고려한 지연 판정 적용
    - 라운드 승자 및 무승부 처리 추가
    - Playing 중 참가자 퇴장을 탈락으로 처리
    - 라운드 종료 중복 방지 및 서버 로그 추가

--- Source/Project48/Game/P48SurvivalGameMode.cpp
+++ Source/Project48/Game/P48SurvivalGameMode.cpp
@@ -1,13 +1,232 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// P48SurvivalGameMode.cpp
 
 
 #include "P48SurvivalGameMode.h"
 
-void AP48SurvivalGameMode::PrepareNextRound()
+#include "P48GameStateBase.h"
+#include "Project48/Character/P48PlayerState.h"
+
+
+int32 AP48SurvivalGameMode::GetAliveParticipantCount() const
 {
-	/**
-	 * TODO: 플레이어 생존 상태 초기화, 관전 상태 해제, 플레이어 재스폰
-	 */
+	const AP48GameStateBase* P48GameState = GetGameState<AP48GameStateBase>();
+	if (IsValid(P48GameState) == false)
+	{
+		return 0;
+	}
 	
-	Super::PrepareNextRound();
+	int32 AliveCount = 0;
+	
+	for (APlayerState* PlayerState : P48GameState->PlayerArray)
+	{
+		const AP48PlayerState* P48PlayerState = Cast<AP48PlayerState>(PlayerState);
+		if (IsValid(P48PlayerState)
+			&& P48PlayerState->IsMatchParticipant()
+			&& P48PlayerState->IsAlive())
+		{
+			AliveCount++;
+		}
+	}
+	
+	return AliveCount;
+}
+
+AP48PlayerState* AP48SurvivalGameMode::LastAliveParticipant() const
+{
+	const AP48GameStateBase* P48GameState = GetGameState<AP48GameStateBase>();
+	if (IsValid(P48GameState) == false)
+	{
+		return nullptr;
+	}
+
+	AP48PlayerState* LastAlivePlayer = nullptr;
+	int32 AliveCount = 0;
+
+	for (APlayerState* PlayerState : P48GameState->PlayerArray)
+	{
+		AP48PlayerState* P48PlayerState = Cast<AP48PlayerState>(PlayerState);
+		if (IsValid(P48PlayerState)
+			&& P48PlayerState->IsMatchParticipant()
+			&& P48PlayerState->IsAlive())
+		{
+			LastAlivePlayer = P48PlayerState;
+			AliveCount++;
+		}
+	}
+
+	return AliveCount == 1 ? LastAlivePlayer : nullptr;
+}
+
+void AP48SurvivalGameMode::NotifyPlayerEliminated(AP48PlayerState* EliminatedPlayer)
+{
+	if (HasAuthority() == false)
+	{
+		return;
+	}
+
+	AP48GameStateBase* P48GameState = GetGameState<AP48GameStateBase>();
+	if (IsValid(P48GameState) == false
+		|| P48GameState->MatchPhase != EP48MatchPhase::Playing)
+	{
+		return;
+	}
+
+	if (IsValid(EliminatedPlayer) == false
+		|| EliminatedPlayer->IsMatchParticipant() == false
+		|| EliminatedPlayer->IsAlive() == false)
+	{
+		return;
+	}
+
+	EliminatedPlayer->SetAlive(false);
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("[Server] Player eliminated: %s, Alive participants: %d"),
+		*EliminatedPlayer->GetPlayerName(),
+		GetAliveParticipantCount());
+
+	RoundEndCheck();
+}
+
+void AP48SurvivalGameMode::RoundEndCheck()
+{
+	if (bRoundEndCheck)
+	{
+		return;
+	}
+
+	bRoundEndCheck = true;
+
+	GetWorldTimerManager().SetTimer(
+		RoundEndCheckTimerHandle,
+		this,
+		&ThisClass::CheckRoundEndCondition,
+		RoundEndCheckDelay,
+		false);
+}
+
+void AP48SurvivalGameMode::CheckRoundEndCondition()
+{
+	if (HasAuthority() == false)
+	{
+		bRoundEndCheck = false;
+		return;
+	}
+
+	AP48GameStateBase* P48GameState = GetGameState<AP48GameStateBase>();
+
+	if (IsValid(P48GameState) == false || P48GameState->MatchPhase != EP48MatchPhase::Playing)
+	{
+		bRoundEndCheck = false;
+		return;
+	}
+
+	const int32 AliveCount = GetAliveParticipantCount();
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("[Server] Round end check: Alive participants = %d"),
+		AliveCount);
+
+	if (AliveCount > 1)
+	{
+		bRoundEndCheck = false;
+		return;
+	}
+
+	FinishSurvivalRound(AliveCount == 1 ? LastAliveParticipant() : nullptr);
+}
+
+void AP48SurvivalGameMode::FinishSurvivalRound(
+	AP48PlayerState* Winner)
+{
+	AP48GameStateBase* P48GameState = GetGameState<AP48GameStateBase>();
+	if (HasAuthority() == false
+		|| IsValid(P48GameState) == false
+		|| P48GameState->MatchPhase != EP48MatchPhase::Playing)
+	{
+		return;
+	}
+
+	if (IsValid(Winner))
+	{
+		Winner->AddRoundWin();
+
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("[Server] Round winner: %s"),
+			*Winner->GetPlayerName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[Server] Round ended in a draw"));
+	}
+
+	StartRoundEnd();
+}
+
+void AP48SurvivalGameMode::ResetParticipantsForRound()
+{
+	AP48GameStateBase* P48GameState = GetGameState<AP48GameStateBase>();
+	if (IsValid(P48GameState) == false)
+	{
+		return;
+	}
+
+	for (APlayerState* PlayerState : P48GameState->PlayerArray)
+	{
+		AP48PlayerState* P48PlayerState = Cast<AP48PlayerState>(PlayerState);
+		if (IsValid(P48PlayerState))
+		{
+			P48PlayerState->ResetForNewRound();
+		}
+	}
+}
+
+void AP48SurvivalGameMode::StartMatch()
+{
+	if (HasAuthority() == false)
+	{
+		return;
+	}
+
+	GetWorldTimerManager().ClearTimer(RoundEndCheckTimerHandle);
+	bRoundEndCheck = false;
+
+	ResetParticipantsForRound();
+	Super::StartMatch();
+
+	UE_LOG(
+		LogTemp,
+		Warning,
+		TEXT("[Server] Survival round started: Alive participants = %d"),
+		GetAliveParticipantCount());
+}
+
+void AP48SurvivalGameMode::Logout(AController* Exit)
+{
+	AP48GameStateBase* P48GameState = GetGameState<AP48GameStateBase>();
+
+	AP48PlayerState* ExitingPlayerState = IsValid(Exit) ? Exit->GetPlayerState<AP48PlayerState>() : nullptr;
+
+	if (IsValid(P48GameState)
+		&& P48GameState->MatchPhase == EP48MatchPhase::Playing
+		&& IsValid(ExitingPlayerState)
+		&& ExitingPlayerState->IsMatchParticipant()
+		&& ExitingPlayerState->IsAlive())
+	{
+		UE_LOG(
+			LogTemp,
+			Warning,
+			TEXT("[Server] Participant logout treated as elimination: %s"),
+			*ExitingPlayerState->GetPlayerName());
+
+		NotifyPlayerEliminated(ExitingPlayerState);
+	}
+
+	Super::Logout(Exit);
 }
