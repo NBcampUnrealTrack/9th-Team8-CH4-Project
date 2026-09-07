96b4ad8e47e53b418ce4e241df37df135bd9a140
variable <jegalvariable@gmail.com>
2026-09-07 11:19:10 +0900

    add: P48InputNicknameWidget, P48MainMenuWidget, MainMenuLevel 추가 (#72)

---
+++ Source/Project48/UI/MainMenu/P48InputNicknameWidget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "P48InputNicknameWidget.generated.h"
+
+UCLASS()
+class PROJECT48_API UP48InputNicknameWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
