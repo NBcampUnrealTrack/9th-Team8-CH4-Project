e1173a28d2f3b708f189b504d256b08ad7189a17
variable <jegalvariable@gmail.com>
2026-09-07 18:22:01 +0900

    add: HSLevel, HSGameModeBase 추가 (#72)

---
+++ Source/Project48/HSGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Game/P48GameModeBase.h"
+#include "HSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT48_API AHSGameModeBase : public AP48GameModeBase
+{
+	GENERATED_BODY()
+	
+};
