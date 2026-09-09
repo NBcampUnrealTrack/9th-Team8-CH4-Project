294377ab03eea3ca04588483a4ac992b4ee196e7
variable <jegalvariable@gmail.com>
2026-09-10 01:51:59 +0900

    perf: 그로기 게이지 & 닉네임 UI 수정 (#72)

--- Source/Project48/UI/P48PlayerNameWidgetComponent.cpp
+++ Source/Project48/UI/P48PlayerNameWidgetComponent.cpp
@@ -2,5 +2,25 @@
 
 UP48PlayerNameWidgetComponent::UP48PlayerNameWidgetComponent()
 {
-	SetWidgetSpace(EWidgetSpace::Screen);
+	PrimaryComponentTick.bCanEverTick = true;
+	//PrimaryComponentTick.TickInterval = 0.05f;
+	
+	SetWidgetSpace(EWidgetSpace::World);
+}
+
+void UP48PlayerNameWidgetComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	if (IsValid(PC) == false) return;
+	
+	FVector CamLoc;
+	FRotator CamRot;
+	PC->GetPlayerViewPoint(CamLoc, CamRot);
+
+	const FVector ToCamera = CamLoc - GetComponentLocation();
+	const FRotator LookAtRot = ToCamera.Rotation();
+		
+	SetWorldRotation(LookAtRot);
 }
