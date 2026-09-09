ab97c5163cbd3a6d84f542e5e405be5f943aa4bc
variable <jegalvariable@gmail.com>
2026-09-09 19:34:53 +0900

    feat: 그로기 게이지 UI 구현 (#72)

--- Source/Project48/UI/P48PlayerNameWidget.h
+++ Source/Project48/UI/P48PlayerNameWidget.h
@@ -5,6 +5,7 @@
 #include "P48PlayerNameWidget.generated.h"
 
 class UTextBlock;
+class UProgressBar;
 
 UCLASS()
 class PROJECT48_API UP48PlayerNameWidget : public UUserWidget
@@ -12,10 +13,16 @@ class PROJECT48_API UP48PlayerNameWidget : public UUserWidget
 	GENERATED_BODY()
 	
 public:
+	virtual void NativeConstruct() override;
+	
 	UFUNCTION(BlueprintCallable)
 	void SetPlayerName(const FString& InPlayerName);
+	UFUNCTION(BlueprintCallable)
+	void UpdateGroggy(float CurrentGroggy, float MaxGroggy);
 
 protected:
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UTextBlock> TextBlock_PlayerNameText;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar_GroggyGauge;
 };
--- Source/Project48/UI/P48PlayerNameWidgetComponent.cpp
+++ Source/Project48/UI/P48PlayerNameWidgetComponent.cpp
@@ -2,5 +2,5 @@
 
 UP48PlayerNameWidgetComponent::UP48PlayerNameWidgetComponent()
 {
-	SetWidgetSpace(EWidgetSpace::World);
+	SetWidgetSpace(EWidgetSpace::Screen);
 }
