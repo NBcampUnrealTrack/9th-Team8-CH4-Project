c998fba874affab1686cf3bdb3eb99fdf5be6c74
문승종 <jongcc2345@gmail.com>
2026-09-10 11:29:47 +0900

    feat: GameplayMessageLibrary 단순 이벤트 송수신 지원

--- Source/Project48/GameplayMessageLibrary/Core/P48GameplayMessageLibrary.h
+++ Source/Project48/GameplayMessageLibrary/Core/P48GameplayMessageLibrary.h
@@ -3,6 +3,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/GameplayMessageSubsystem.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "P48GameplayMessagePayloads.h"
 #include "P48GameplayMessageLibrary.generated.h"
 
 UCLASS()
@@ -11,6 +12,12 @@ class PROJECT48_API UP48GameplayMessageLibrary : public UBlueprintFunctionLibrar
 	GENERATED_BODY()
 
 public:
+	/** Payload 없이 채널의 사건 발생만 알립니다. */
+	UFUNCTION(BlueprintCallable, Category = "Gameplay Message", meta = (WorldContext = "WorldContextObject"))
+	static bool BroadcastEvent(
+		const UObject* WorldContextObject,
+		FGameplayTag Channel);
+
 	template <typename TPayload>
 	static bool Broadcast(
 		const UObject* WorldContextObject,
@@ -53,6 +60,37 @@ public:
 		Params.SetMessageReceivedCallback(Listener, Handler);
 
 		return UGameplayMessageSubsystem::Get(Listener).RegisterListener<TPayload>(Channel, Params);
+	}
+
+	/** Payload를 받지 않는 멤버 함수로 단순 이벤트 채널을 구독합니다. */
+	template <typename TListener>
+	static FGameplayMessageListenerHandle ListenEvent(
+		TListener* Listener,
+		const FGameplayTag Channel,
+		void (TListener::*Handler)(FGameplayTag),
+		const EGameplayMessageMatch MatchType = EGameplayMessageMatch::ExactMatch)
+	{
+		if (!IsValid(Listener) || !Channel.IsValid() || Handler == nullptr)
+		{
+			return FGameplayMessageListenerHandle();
+		}
+
+		if (!UGameplayMessageSubsystem::HasInstance(Listener))
+		{
+			return FGameplayMessageListenerHandle();
+		}
+
+		TWeakObjectPtr<TListener> WeakListener(Listener);
+		return UGameplayMessageSubsystem::Get(Listener).RegisterListener<FP48EmptyMessage>(
+			Channel,
+			[WeakListener, Handler](const FGameplayTag ActualChannel, const FP48EmptyMessage&)
+			{
+				if (TListener* StrongListener = WeakListener.Get())
+				{
+					(StrongListener->*Handler)(ActualChannel);
+				}
+			},
+			MatchType);
 	}
 
 	static bool StopListening(FGameplayMessageListenerHandle& Handle);
