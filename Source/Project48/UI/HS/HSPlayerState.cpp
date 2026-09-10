8a0dfed3c5735e58fd075384016ec8859632d8b6
variable <jegalvariable@gmail.com>
2026-09-10 15:54:15 +0900

    add: HSPlayerState 추가 (#103)

---
+++ Source/Project48/UI/HS/HSPlayerState.cpp
@@ -0,0 +1,2 @@
+#include "HSPlayerState.h"
+
---
+++ Source/Project48/UI/HS/HSPlayerState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Project48/Character/P48PlayerState.h"
+#include "HSPlayerState.generated.h"
+
+UCLASS()
+class PROJECT48_API AHSPlayerState : public AP48PlayerState
+{
+	GENERATED_BODY()
+	
+};
