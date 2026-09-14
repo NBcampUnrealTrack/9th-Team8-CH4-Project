bb31b4bed38cbcf480f04d42032e697bc28bbc20
variable <jegalvariable@gmail.com>
2026-09-14 16:37:22 +0900

    add: RoundWidget 파일 추가 (#160)

---
+++ Source/Project48/UI/P48RoundWidget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "P48RoundWidget.generated.h"
+
+UCLASS()
+class PROJECT48_API UP48RoundWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
