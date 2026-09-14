a4082583f3125fb882c680b32dbcef2de9d20af8
AntiFreeze81 <wnsgud3590@naver.com>
2026-09-15 04:34:20 +0900

    feat: add-grab-throw-character

--- Source/Project48/Character/P48PlayerCharacter.h
+++ Source/Project48/Character/P48PlayerCharacter.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
@@ -30,6 +30,7 @@ public:
 	virtual void Destroyed() override;
 	
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void Tick(float DeltaSeconds) override;
 protected:
 	virtual void BeginPlay() override;
 	virtual void PossessedBy(AController* NewController) override;
@@ -98,6 +99,19 @@ protected:
 	UFUNCTION()
 	void OnRep_Weapon();
 
+	//Carry
+	UPROPERTY(ReplicatedUsing = OnRep_CarriedCharacter)
+	TObjectPtr<AP48PlayerCharacter> CarriedCharacter;
+
+	UPROPERTY(ReplicatedUsing = OnRep_CarrierCharacter)
+	TObjectPtr<AP48PlayerCharacter> CarrierCharacter;
+
+	UFUNCTION()
+	void OnRep_CarriedCharacter();
+
+	UFUNCTION()
+	void OnRep_CarrierCharacter();
+
 	//Input Block
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Input")
 	bool bInputBlocked = false;
@@ -128,6 +142,17 @@ public:
 	
 	//Weapon
 	FORCEINLINE class AP48WeaponBase* GetEquippedWeapon() const { return Weapon; }
+	
+	//Carry / Throw
+	FORCEINLINE bool IsCarryingCharacter() const { return CarriedCharacter != nullptr; }
+	FORCEINLINE bool IsBeingCarried() const { return CarrierCharacter != nullptr; }
+	FORCEINLINE AP48PlayerCharacter* GetCarriedCharacter() const { return CarriedCharacter; }
+	FORCEINLINE AP48PlayerCharacter* GetCarrierCharacter() const { return CarrierCharacter; }
+	bool IsAlive() const;
+
+	void OnPickedUpBy(AP48PlayerCharacter* InCarrier);
+	void OnThrown(const FVector& ReleaseLocation, const FRotator& ReleaseRotation, const FVector& ThrowVelocity, AP48PlayerCharacter* InCarrier);
+	void OnDroppedFromCarrier(const FVector& DropLocation, const FRotator& DropRotation, AP48PlayerCharacter* InCarrier);
 	
 	
 private:
@@ -207,6 +232,15 @@ private:
 	
 	UFUNCTION(Server, Reliable)
 	void Server_DropWeapon();
+
+	UFUNCTION(Server, Reliable)
+	void Server_PickUpCharacter(AP48PlayerCharacter* TargetCharacter);
+
+	UFUNCTION(Server, Reliable)
+	void Server_ThrowCharacter();
+
+	UFUNCTION(Server, Reliable)
+	void Server_DropCharacter();
 	
 	//MulticastRPC
 	UFUNCTION(NetMulticast, Unreliable)
@@ -220,6 +254,79 @@ private:
 	
 	UFUNCTION(NetMulticast, Reliable)
 	void Multicast_DeathRagDoll();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_PlayThrowMontage();
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_OnThrown(const FVector& ReleaseLocation, const FRotator& ReleaseRotation, const FVector& ThrowVelocity, AP48PlayerCharacter* InCarrier);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_OnDropped(const FVector& DropLocation, const FRotator& DropRotation, AP48PlayerCharacter* InCarrier);
+
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_RecoverFromRagdoll(const FVector& StandLocation, const FRotator& StandRotation);
+
+	void Server_RecoverFromRagdoll();
+
+	FName GetRagdollRootBoneName() const;
+
+	//Carry Settings
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	FName CarrySocketName = TEXT("handslot_r");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	float ThrowForwardSpeed = 1800.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	float ThrowUpSpeed = 600.0f;
+
+	// 던져진 후 바닥에 누워있는 래그돌 지속 시간 (초)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	float RagdollRecoverDuration = 1.5f;
+
+	// 래그돌에서 일어날 때 재생할 몽타주 (선택 사항)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry|Animation", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UAnimMontage> GetUpMontage;
+
+	// 캐리어 기준 전방 오프셋 (cm)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	float CarriedForwardOffset = 15.0f;
+
+	// 캐리어 기준 우측 오프셋 (cm)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	float CarriedRightOffset = 0.0f;
+
+	// 캐리어 손 기준 높이 오프셋 (cm, 음수면 손 아래로 캡슐 중심이 위치하여 멱살/목덜미를 잡는 모양)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	float CarriedHeightOffset = -40.0f;
+
+	// 캐리어 기준 Yaw 회전 오프셋 (0이면 정면, 180이면 캐리어를 마주봄)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	float CarriedYawOffset = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UAnimMontage> ThrowMontage;
+
+	// 들려있는 동안 래그돌 물리 시뮬레이션 활성화 여부
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry|Physics", meta=(AllowPrivateAccess="true"))
+	bool bEnableCarriedRagdoll = true;
+
+	// 들려있는 동안 물리 블렌딩 비율 (0.0 = 완전 뻣뻣함, 1.0 = 완전 래그돌, 0.5~0.6 = 자연스럽게 덜렁거림)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry|Physics", meta=(AllowPrivateAccess="true", ClampMin="0.0", ClampMax="1.0"))
+	float CarriedPhysicsBlendWeight = 0.6f;
+
+	// 래그돌을 시뮬레이션할 기준 본 이름 (pelvis는 루트이므로 이탈 방지를 위해 spine 권장)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry|Physics", meta=(AllowPrivateAccess="true"))
+	FName CarriedPhysicsBoneName = TEXT("spine");
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Carry|Debug", meta=(AllowPrivateAccess="true"))
+	bool bEnableCarryDebug = true;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Carry", meta=(AllowPrivateAccess="true"))
+	bool bIsInThrownRagdoll = false;
+
+	FTimerHandle RagdollRecoverTimerHandle;
 	
 	//attack/hit
 	UPROPERTY()
@@ -239,4 +346,5 @@ private:
 public:
 	virtual void Jump() override;
 	virtual void OnJumped_Implementation() override;
+	virtual void Landed(const FHitResult& Hit) override;
 };
