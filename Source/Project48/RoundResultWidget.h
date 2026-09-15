e891794f453196d7cce9a4525c433a18b48c896c
variable <jegalvariable@gmail.com>
2026-09-15 15:11:10 +0900

    라운드결과 위젯 추가, 유저ID입력창 글자색 수정 (#160)

---
+++ Source/Project48/RoundResultWidget.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RoundResultWidget.generated.h"
+
+UCLASS()
+class PROJECT48_API URoundResultWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
