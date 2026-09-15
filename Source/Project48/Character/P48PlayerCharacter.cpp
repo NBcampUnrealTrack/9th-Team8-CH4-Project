c665f52267dede3985f86f26f00b4bc855c1bec5
variable <jegalvariable@gmail.com>
2026-09-16 06:13:41 +0900

    닉네임 업데이트 시점 변경 (#192)

--- Source/Project48/Character/P48PlayerCharacter.cpp
+++ Source/Project48/Character/P48PlayerCharacter.cpp
@@ -223,6 +223,11 @@ void AP48PlayerCharacter::BeginPlay()
 		StartRot.Pitch = -45.0f;
 		Controller->SetControlRotation(StartRot);
 	}
+	
+	if (NicknameWidgetComponent)
+	{
+		NicknameWidgetComponent->UpdateNickname();
+	}
 }
 
 void AP48PlayerCharacter::PossessedBy(AController* NewController)
